ec9586b9b7782c339641965d943790d7c2c24466
jsmccarver <jared.mccarver@gmail.com>
2022-02-01 16:08:51 -0500

    Addition of Pet rooms

--- base/views.py
+++ base/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 # Create your views here.
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-]
 
 def home(request):
+    rooms = [
+        {'id': 1, 'name': 'Lets learn python!'},
+        {'id': 2, 'name': 'Design with me'},
+        {'id': 3, 'name': 'Frontend developers'},
+    ]
     context = {'rooms' : rooms}
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
     
-def rooms(request):
-    context = {'rooms' : rooms}
-    return render(request, "room.html", context)
+def pets(request, pk):
+    context = {
+        "first_name" : "Naveen",
+        "last_name"  : "Arora",
+    }
+    return render(request, "base/pets.html", context)
