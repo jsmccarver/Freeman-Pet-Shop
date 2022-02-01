644b9efe7659e3215da4275376fc6389e428e79c
jsmccarver <jared.mccarver@gmail.com>
2022-02-01 11:34:15 -0500

    Hide Secret Key

--- petstore/settings.py
+++ petstore/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f2&1llhqg5$&s1lvje-7-#@m0+#z&#lvfxlr&ty9ruo0+)+veo'
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
