31665261d76fc66165ec9d4a5ef6106f9ebaab10
jsmccarver <jared.mccarver@gmail.com>
2022-02-01 13:36:31 -0500

    ssl

--- petstore/settings.py
+++ petstore/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 
 ROOT_URLCONF = 'petstore.urls'
