dff841402aecaad065608d35390697f86c3cd8ab
jsmccarver <jared.mccarver@gmail.com>
2022-02-03 09:01:27 -0500

    debug true

--- petstore/settings.py
+++ petstore/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['freeman-pet-shop.herokuapp.com', '127.0.0.1', 'localhost', 'freemanpets.com']
 
