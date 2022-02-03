1194ef7385f9eee96aeb59a72b01a5d75217d6ae
jsmccarver <jared.mccarver@gmail.com>
2022-02-03 08:58:30 -0500

    add postgres

--- petstore/settings.py
+++ petstore/settings.py
@@ -87,6 +87,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+import dj_database_url
+db_From_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
