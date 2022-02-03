f019dca66545a19058fa1fd349ae2b637baa5587
jsmccarver <jared.mccarver@gmail.com>
2022-02-03 08:23:22 -0500

    commit

--- petstore/settings.py
+++ petstore/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+ADMIN = [('Jared McCarver', 'jared.mccarver@gmail.com')]
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
