992e86d03fc5fe37d9f1ee93ff48ef920d9bd75d
jsmccarver <jared.mccarver@gmail.com>
2022-03-21 11:00:48 -0400

    fix routes

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.getRoutes),
-    path('/pets', views.getPets),
+    path('pets', views.getPets),
     path('add', views.addPet),
 ]
