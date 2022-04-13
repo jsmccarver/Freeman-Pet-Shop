d016d16633ab49ecc80c59b28adf0992819b29ff
jsmccarver <jared.mccarver@gmail.com>
2022-04-13 16:27:43 -0400

    add static root

--- petstore/settings.py
+++ petstore/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # AWS BUCKET
